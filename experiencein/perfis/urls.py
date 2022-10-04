19ebdc82f6db60e2e98ed15c785912dcdeb086e2
ErikaSisan <erika.santos@estudante.ifb.edu.br>
2022-10-04 17:54:56 -0300

    atualização

---
+++ experiencein/perfis/urls.py
@@ -0,0 +1,8 @@
+# experiencein/perfis/urls.py 
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('perfis/<int:perfil_id>', views.exibir, name='exibir'),
+]
--- experiencein/perfis/views.py
+++ experiencein/perfis/views.py
@@ -1,3 +1,19 @@
+# experiencein/perfis/views.py 
 from django.shortcuts import render
+from perfis.models import Perfil
+
+def index(request):
+    return render(request, 'index.html')
+
+# experiencein/perfis/views.py 
+
+# código anterior omitido
+
+def exibir(request, perfil_id):
+
+    perfil = Perfil.objects.get(id=perfil_id)
+    return render(request, 'perfil.html', { "perfil" : perfil})
 
 # Create your views here.
+
+
