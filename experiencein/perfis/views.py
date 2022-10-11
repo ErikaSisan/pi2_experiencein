8ac59ade4344c4b79e5966572ffdded1fadbf421
ErikaSisan <erika.santos@estudante.ifb.edu.br>
2022-10-11 17:43:00 -0300

    atualizado

--- experiencein/perfis/views.py
+++ experiencein/perfis/views.py
@@ -3,17 +3,22 @@ from django.shortcuts import render
 from perfis.models import Perfil
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', { 'perfis' : Perfil.objects.all()})
 
-# experiencein/perfis/views.py 
-
-# código anterior omitido
 
 def exibir(request, perfil_id):
 
     perfil = Perfil.objects.get(id=perfil_id)
     return render(request, 'perfil.html', { "perfil" : perfil})
 
-# Create your views here.
+def convidar(request, perfil_id):
+
+    perfil_a_convidar = Perfil.objects.get(id=perfil_id)
+    perfil_logado = get_perfil_logado(request)
+    perfil_logado.convidar(perfil_a_convidar)
+    return render(request, 'index.html', {'perfis' : Perfil.objects.all()})
+
+def get_perfil_logado(request):
+    return Perfil.objects.get(id=3) 
 
 
