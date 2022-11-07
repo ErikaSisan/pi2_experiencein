7d0c8dafbbce022ffe45efddd4656d0f25ed47a5
ErikaSisan <erika.santos@estudante.ifb.edu.br>
2022-11-07 19:21:39 -0300

    Módulo 9 Atualizado

--- experiencein/perfis/urls.py
+++ experiencein/perfis/urls.py
@@ -1,8 +1,10 @@
 # experiencein/perfis/urls.py
 from django.urls import path
 from perfis import views
+
 urlpatterns = [
-	path('', views.index, name='index'),
-	path('perfis/<int:perfil_id>', views.exibir, name='exibir'),
-	path('perfis/<int:perfil_id>/convidar', views.convidar, name='convidar')
+ path('', views.index, name='index'),
+ path('perfis/<int:perfil_id>', views.exibir, name='exibir'),
+ path('perfis/<int:perfil_id>/convidar', views.convidar, name='convidar'),
+ path('convite/<int:convite_id>/aceitar', views.aceitar, name='aceitar')
 ]
